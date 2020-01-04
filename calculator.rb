134e23c0980bd5d9553408f4310aea1455ff7cba
Franco Canedo <francanedo1@hotmail.com>
2020-01-04 21:39:59 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,7 @@
 # Add your variables here
+
 first_number = 9
 second_number = 4
 sum = first_number + second_number
 difference = first_number - second_number
+product = first_number * second_number
